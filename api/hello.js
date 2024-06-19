f00dcf66d032bf3b5459b268a44da05a3740c51f
Choi jin woo <66509568+wlsgudy3@users.noreply.github.com>
2024-06-19 20:54:14 +0900

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -2,30 +2,51 @@ const { spawn } = require('child_process');
 const path = require('path');
 
 function _runWasm(reqBody) {
-  return new Promise(resolve => {
-    const wasmedge = spawn(path.join(__dirname, 'wasmedge'), [path.join(__dirname, 'wasmedge_manager.so')]);
+  return new Promise((resolve, reject) => {
+    const wasmedge = spawn(path.join(__dirname, 'wasmedge'), ['--dir', '.', path.join(__dirname, 'wasmedge_manager.so')]);
 
     let d = [];
     wasmedge.stdout.on('data', (data) => {
       d.push(data);
     });
 
-    wasmedge.on('close', (code) => {
-      let buf = Buffer.concat(d);
-      resolve(buf);
+    wasmedge.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
     });
+
+    wasmedge.on('close', (code) => {
+      if (code !== 0) {
+        reject(`wasmedge process exited with code ${code}`);
+      } else {
+        let buf = Buffer.concat(d);
+        resolve(buf);
+      }
+    });
+
+    if (reqBody) {
+      wasmedge.stdin.write(reqBody);
+    }
+    wasmedge.stdin.end();
   });
 }
 
 exports.handler = async function(event, context) {
-  let buf = await _runWasm();
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT"
-    },
-    body: buf.toString()
-  };
-}
+  try {
+    let buf = await _runWasm(event.body);
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT"
+      },
+      body: buf.toString()
+    };
+  } catch (error) {
+    console.error(`Error: ${error}`);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
+};
